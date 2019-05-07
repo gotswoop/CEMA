0266c444bb73c06c8746abd900bd5a3ae33dec51
Swaroop Samek <swaroop.samek@gmail.com>
2019-05-07 12:25:15 -0700

    ux changes to new user details layout

--- subjects/forms.py
+++ subjects/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class SMS_Form(forms.Form):
-	sms_message = forms.CharField(label='Type Text Message', max_length=160)
+	sms_message = forms.CharField(label='Message Body:', max_length=160)

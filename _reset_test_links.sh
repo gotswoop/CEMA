006b1c4f72aed44ec6796f5830dbf9c9ccc93766
Swaroop Samek <swaroop.samek@gmail.com>
2019-06-05 14:22:26 -0700

    disabling "_reset_test_links.sh" cron as we now have a testing feature

--- _reset_test_links.sh
+++ _reset_test_links.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mysql -s -r -e 'UPDATE uscstudy.survey_links SET start_datetime = NOW(), status = 0, last_answered_question = null WHERE survey_key LIKE "%-time" OR survey_key LIKE "%-risk"'
+# No longer used as survey Testing code is in place.
+# mysql -s -r -e 'UPDATE uscstudy.survey_links SET start_datetime = NOW(), status = 0, last_answered_question = null WHERE survey_key LIKE "%-time" OR survey_key LIKE "%-risk"'

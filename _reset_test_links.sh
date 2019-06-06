b276ef3c1d5aecb71cceaae4cca75b6a335091b1
Swaroop Samek <swaroop.samek@gmail.com>
2019-06-05 19:10:38 -0700

    Removed _reset_test_link.sh as we no longer need to reset custom survey links such as swoop-test etc

--- _reset_test_links.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# No longer used as survey Testing code is in place.
-# mysql -s -r -e 'UPDATE uscstudy.survey_links SET start_datetime = NOW(), status = 0, last_answered_question = null WHERE survey_key LIKE "%-time" OR survey_key LIKE "%-risk"'

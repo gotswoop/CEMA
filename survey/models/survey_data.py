d5155d893a70db0c2afcc258675c98e9764b1580
Swaroop Samek <swaroop.samek@gmail.com>
2019-09-13 10:34:11 -0500

    Changed max len of response file in survey_data from 50 to 150 to accomodate extra long responses

--- survey/models/survey_data.py
+++ survey/models/survey_data.py
@@ -4,7 +4,7 @@ from .survey_links import SurveyLinks
 class SurveyData(models.Model):
 	survey_link = models.ForeignKey(SurveyLinks, db_column='survey_link', on_delete=models.PROTECT)
 	question = models.CharField(max_length=50)
-	response = models.CharField(max_length=50)
+	response = models.CharField(max_length=150)
 	user_ip = models.CharField(max_length=15)
 	ts_response = models.DateTimeField(auto_now_add=True)
 	
